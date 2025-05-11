2dd9f3a0f2cb175a433a78d2eceb6526a7798124
bestcb2333 <2892709432@qq.com>
2025-05-11 09:44:43 +0800

    5/11

--- backend/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-go build -o climate . &&
-PORT=8701 JWT_KEY=114514 DB_USER=climate_sentinel DB_NAME=climate_sentinel DB_PASSWORD=climate_sentinel SMTP_MAIL=floodguard@126.com SMTP_PASSWORD=NTbKihEAKrNLcuJm SMTP_SERVER=smtp.126.com SMTP_PORT=25 ./climate
